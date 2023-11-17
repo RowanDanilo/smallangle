851dc78f809567b92895a3803cafd23cf3a90cb4
RowanDanilo <136341394+RowanDanilo@users.noreply.github.com>
2023-11-17 16:06:40 +0100

    just did smallangle

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,13 +4,34 @@ from numpy import pi
 import pandas as pd
 
 
+
+@click.group()
+def cmd_group():
+    pass
+@cmd_group.command()
+
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+)
 def sin(number):
+    """sinus in a table
+
+    Args:
+        number (int): the amount of rows between zero and two pi
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +40,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
