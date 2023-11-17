709d2fdb21036c7466c919cff06b64f1729df125
RowanDanilo <136341394+RowanDanilo@users.noreply.github.com>
2023-11-17 16:36:56 +0100

    adding the docstrings and help function

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -7,24 +7,30 @@ import pandas as pd
 
 @click.group()
 def cmd_group():
+    """With Smallangle you can calculate the sinus and tangent of different values between zero and 2 pi.
+    """    
     pass
-@cmd_group.command()
 
+
+@cmd_group.command()
 @click.option(
     "-n",
     "--number",
     default=10,
+    help="the amount of rows between zero and two pi",
+    show_default=True
 )
 def sin(number):
-    """sinus in a table
+    """Sine in a table.
 
     Args:
-        number (int): the amount of rows between zero and two pi
+        number (int): The amount of rows between zero and two pi.
     """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
+
 
 @cmd_group.command()
 @click.option(
@@ -33,11 +39,15 @@ def sin(number):
     default=10,
 )
 def tan(number):
+    """Tangent in a table.
+
+    Args:
+        number (int): The amount of rows between zero and two pi.
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
-
 
 if __name__ == "__main__":
     cmd_group()
